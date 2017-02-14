90faa223281841a632395ebe545cedf3517ad1a7
nli006 <nli006@ucr.edu>
2017-02-13 20:13:01 -0800

    equivalence of node not working

--- 8puzzle/src/menu.java
+++ 8puzzle/src/menu.java
@@ -7,6 +7,7 @@ public class menu {
 	public void display(){
 		Scanner input = new Scanner(System.in);
 		String selection = "";
+		node puzzle = null;
 		System.out.println("Welcome to 861102031's 8-puzzle solver. \n"
 				+ "Type \"1\" to use a default puzzle, or \"2\" to enter your own puzzle."
 				+ "\n");
@@ -14,26 +15,41 @@ public class menu {
 		while (!selection.equals("1") && !selection.equals("2")){
 			selection = input.nextLine();
 			if (selection.equals("1")){
-				input.close();
-				defaultPuzzle();
+				puzzle = defaultPuzzle();
 			}
 			else if (selection.equals("2")){
-//				input.close();
-				customPuzzle(input);
-				input.close();
+				puzzle = customPuzzle(input);
 			}
 			else System.out.println("Invalid selection. Try again.");
 		}
+		
+		System.out.println("Enter choice of algorithm: \n"
+				+ "1. Uniform Cost Search \n"
+				+ "2. A* with Misplaced Tile heuristic \n"
+				+ "3. A* with Manhattan Distance heuristic \n");
+		selection = "";
+		
+		while(!selection.equals("1") && !selection.equals("2") && !selection.equals("3")){
+			selection = input.nextLine();
+			if (selection.equals("1")){
+				puzzle.ucs();
+			}
+			else if (selection.equals("2")){
+				puzzle.mpt();
+			}
+			else if (selection.equals("3")){
+				puzzle.md();
+			}
+		}
 	}
 	
-	public void defaultPuzzle(){
-		List<Integer> row1 = new ArrayList<Integer>(Arrays.asList(1, 2, 3));
-		List<Integer> row2 = new ArrayList<Integer>(Arrays.asList(4, 0, 5));
-		List<Integer> row3 = new ArrayList<Integer>(Arrays.asList(6, 7, 8));
-		node defaultNode = new node(row1, row2, row3);
+	public node defaultPuzzle(){
+		Integer[][] puzzle = new Integer[][]{{1, 2, 3},{4, 0, 5},{6, 7, 8}};
+		node defaultNode = new node(puzzle);
+		return defaultNode;
 	}
 	
-	public void customPuzzle(Scanner input){
+	public node customPuzzle(Scanner input){
 //		Scanner input = new Scanner(System.in);
 		
 		System.out.println("Enter your pizzle, use a zero to represent the blank\n"
@@ -47,21 +63,25 @@ public class menu {
 		String[] srow1 = r1.split(" ");
 		String[] srow2 = r2.split(" ");
 		String[] srow3 = r3.split(" ");
-		
-		System.out.println(Arrays.deepToString(srow1));
-		System.out.println(Arrays.deepToString(srow2));
-		System.out.println(Arrays.deepToString(srow3));
 
-		List<Integer> al_r1 = new ArrayList<Integer>();
-		List<Integer> al_r2 = new ArrayList<Integer>();
-		List<Integer> al_r3 = new ArrayList<Integer>();
+		Integer[] al_r1 = new Integer[3]; 
+		Integer[] al_r2 = new Integer[3];
+		Integer[] al_r3 = new Integer[3];
 		
-		for(int i = 0; i < srow1.length; ++i){
-			al_r1.add(Integer.parseInt(srow1[i]));
-			al_r2.add(Integer.parseInt(srow2[i]));
-			al_r3.add(Integer.parseInt(srow3[i]));
+		for(Integer i = 0; i < srow1.length; ++i){
+			al_r1[i] = Integer.parseInt(srow1[i]);
+			al_r2[i] = Integer.parseInt(srow2[i]);
+			al_r3[i] = Integer.parseInt(srow3[i]);
 		}
 		
-		node custom = new node(al_r1, al_r2, al_r3);
+		Integer[][] puzzle = new Integer[3][3];
+		for(Integer i = 0; i < 3; ++i){
+			puzzle[0][i] = al_r1[i];
+			puzzle[1][i] = al_r2[i];
+			puzzle[2][i] = al_r3[i];
+		}
+		
+		node custom = new node(puzzle);
+		return custom;
 	}
 }
--- 8puzzle/src/node.java
+++ 8puzzle/src/node.java
@@ -1,36 +1,247 @@
+import java.awt.List;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class node {
-	List<List<Integer>> puzzle = new ArrayList<List<Integer>>();
+	Integer [][] puzzle = new Integer[3][3];
 	node up;
 	node down;
 	node right;
 	node left;
+	double f;
+	double g;
+	double h;
+	Integer row;
+	Integer col;
 	
-	node(List<Integer> row1, List<Integer> row2, List<Integer> row3){
-		puzzle.add(row1);
-		puzzle.add(row2);
-		puzzle.add(row3);
-		
+	
+	node(Integer[][] puz){
+		puzzle = puz;
 		up = down = left = right = null;
+	}
+	
+	void print(Integer[][] a){
+		for(Integer i = 0; i < 3; ++i){
+			for(Integer j = 0; j < 3; ++j){
+				System.out.print(a[i][j] + " ");
+			}
+			System.out.print("\n");
+		}
+	}
+	
+	// Finds and sets index of 0 if exists. If does not exist, returns false.
+	boolean find0(Integer[][] a){
+		for (Integer i = 0; i < 3; ++i){
+			for (Integer j = 0; j < 3; ++j){
+				if(puzzle[i][j] == 0){
+					row = i;
+					col = j;
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	boolean addLeft(){
+		find0(puzzle);
+		if(col == 0){
+			left = null;
+			return false;
+		}
+		Integer [][] new_puz = new Integer[3][3];
+		copy(puzzle, new_puz);
 		
-		System.out.println(puzzle);
+		Integer temp = new_puz[row][col - 1];
+		new_puz[row][col - 1] = 0;
+		new_puz[row][col] = temp;
+		
+		node new_left = new node(new_puz); //cost is wrong
+		left = new_left;
+		return true;
 	}
 	
-	void addLeft(node a){
-		left = a;
+	boolean addRight(){
+		find0(puzzle);
+		if(col == 2){
+			right = null;
+			return false;
+		}
+		Integer [][] new_puz = new Integer[3][3];
+		copy(puzzle, new_puz);
+		
+		Integer temp = puzzle[row][col + 1];
+		new_puz[row][col + 1] = 0;
+		new_puz[row][col] = temp;
+		
+		node new_right = new node(new_puz); //cost is wrong
+		right = new_right;
+		return true;
 	}
 	
-	void addRight(node a){
-		right = a;
+	boolean addUp(){
+		find0(puzzle);
+		if(row == 0){
+			up = null;
+			return false;
+		}
+		Integer [][] new_puz = new Integer[3][3];
+		copy(puzzle, new_puz);
+		
+		Integer temp = puzzle[row - 1][col];
+		new_puz[row - 1][col] = 0;
+		new_puz[row][col] = temp;
+		
+		node new_up = new node(new_puz); //cost is wrong
+		up = new_up;
+		return true;
 	}
 	
-	void addUp(node a){
-		up = a;
+	boolean addDown(){
+		find0(puzzle);
+		if(row == 2){
+			down = null;
+			return false;
+		}
+		Integer [][] new_puz = new Integer[3][3];
+		copy(puzzle, new_puz);
+		
+		Integer temp = puzzle[row + 1][col];
+		new_puz[row + 1][col] = 0;
+		new_puz[row][col] = temp;
+		
+		node new_down = new node(new_puz); //cost is wrong
+		down = new_down;
+		return true;
 	}
 	
-	void addDown(node a){
-		down = a;
+	void copy(Integer[][] src, Integer[][] dest){
+		for(Integer i = 0; i < 3; ++i){
+			for(Integer j = 0; j < 3; ++j){
+				dest[i][j] = src[i][j];
+			}
+		}
+	}
+	
+	void expand(){
+		addRight();
+		addLeft();
+		addUp();
+		addDown();
+	}
+	
+	void ucs(){
+		Comparator<node> comparator = new nodeComparator();
+		PriorityQueue<node> q = new PriorityQueue<node>(10, comparator);
+		ArrayList<node> explored = new ArrayList<node>();
+		
+//		Integer test = 0;
+		q.add(this);
+		node temp;
+//		while(test != 2){
+		while(q.size() != 0){
+			temp = q.poll();
+			if(temp.left != null){
+				if(temp.left.checkFinal()){
+					System.out.println("Found solution left");
+					break;
+				}
+				temp.left.f = f + 1;
+				if(!temp.left.checkList(explored)){
+					System.out.println("adding :");
+					print(temp.left.puzzle);
+					q.add(temp.left);
+				}
+			}
+			if(temp.right != null){
+				if(temp.right.checkFinal()){
+					System.out.println("Found solution right");
+					break;
+				}
+				temp.right.f = f + 1;
+				if(!temp.right.checkList(explored)){
+					System.out.println("adding :");
+					print(temp.right.puzzle);
+					q.add(temp.right);
+				}
+			}
+			if(temp.up != null){
+				if(temp.up.checkFinal()){
+					System.out.println("Found solution up");
+					break;
+				}
+				temp.up.f = f + 1;
+				if(!temp.up.checkList(explored)){
+					System.out.println("adding :");
+					print(temp.up.puzzle);
+					q.add(temp.up);
+				}
+			}
+			if(temp.down != null){
+				if(temp.down.checkFinal()){
+					System.out.println("Found solution down");
+					break;
+				}
+				temp.down.f = f + 1;
+				if(!temp.down.checkList(explored)){
+					System.out.println("adding :");
+					print(temp.down.puzzle);
+					q.add(temp.down);
+				}
+			}
+			explored.add(temp);
+			temp.expand();
+			System.out.println("Expanding this node: ");
+			print(temp.puzzle);
+//			++test;
+		}
+		System.out.println("Ran out of node to expands");
+	}
+	
+	void mpt(){
+		
+	}
+	
+	void md(){
+		
+	}
+	
+	boolean checkFinal(){
+		Integer[][] final_puz = new Integer[][]{{1, 2, 3},{4, 5, 6},{7, 8, 0}};
+		for(Integer i = 0; i < 3; ++i){
+			for(Integer j = 0; j < 3; ++j){
+				if(!(puzzle[i][j] == final_puz[i][j])){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	boolean check_equals(Integer[][] a, Integer[][] b){
+		for(Integer i = 0; i < 3; ++i){
+			for(Integer j = 0; j < 3; ++j){
+				if(!(a[i][j] == b[i][j])){
+					System.out.print("does not match");
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	boolean checkList(ArrayList<node> a){
+		System.out.println(a.size());
+		for(Integer i = 0; i < a.size(); ++i){
+			System.out.println(i + ":");
+			print(a.get(i).puzzle);
+			if(check_equals(puzzle, a.get(i).puzzle)){
+				System.out.println("Has been explored");
+				return true;
+			}
+		}
+		return false;
 	}
 }
